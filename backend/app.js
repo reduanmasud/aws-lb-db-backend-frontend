e93693c4ca5ac03981ea83958f1b812db97afda0
Md. Reduan Masud <reduanmasudcse@gmail.com>
2023-10-09 23:17:22 +0600

    Initialize

---
+++ backend/app.js
@@ -0,0 +1,40 @@
+const express = require('express')
+var cors = require('cors')
+
+const app = express()
+const port = 3000
+app.use(cors())
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get("/cities", (req,res) => {
+    const cities = [
+        {
+          id: 1,
+          name: "New York",
+        },
+        {
+          id: 2,
+          name: "Berlin",
+        },
+        {
+          id: 3,
+          name: "London",
+        },
+        {
+          id: 4,
+          name: "Dhaka",
+        },
+        {
+          id: 3,
+          name: "NY",
+        }
+    ];
+  
+    res.json(cities);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
---
+++ frontend/src/App.tsx
@@ -0,0 +1,37 @@
+// @ts-ignore
+import {useEffect, useState } from 'react'
+
+import './App.css'
+
+const host = "http://localhost";
+const port = "3000";
+function App() {
+
+
+  const [cities, setCities] = useState<any[]>([]);  
+  const fetchList = () => {
+    fetch(`${host}:${port}/cities`)
+    .then(res => {
+      console.log(res);
+      return res.json();
+    }).then((data) => setCities(data));
+  }
+
+  useEffect(()=>{
+    fetchList();
+  }, []);
+  console.log(cities);
+
+  return (
+    <>
+    <h1>Hello, !!</h1>
+    <ul>
+     {cities.map((city, idx)=>(
+      <li>{city.name}</li>
+     ))}
+     </ul>
+    </>
+  )
+}
+
+export default App
